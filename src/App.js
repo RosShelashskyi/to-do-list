c585ab099abd9ada36a752ef6afa9d083d3252ab
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-06-07 12:02:39 +0200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id="main">
+        <div className="title">To-Do List</div>
+        <div className="center">
+          <List/>
+        </div>
+      </div>
     </div>
   );
+}
+
+function List(){
+  return(
+    <div>
+
+    </div>
+  )
 }
 
 export default App;
